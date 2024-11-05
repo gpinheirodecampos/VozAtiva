5c2c4fde3e7cab88ab55af954b02715f54969adb
GuiOSousa <guilherme.olivesousa@gmail.com>
2024-11-05 12:49:52 -0300

    Changed DTO from class to record

--- VozAtiva.Application/DTOs/UserDTO.cs
+++ VozAtiva.Application/DTOs/UserDTO.cs
@@ -3,13 +3,11 @@ using VozAtiva.Domain.Enums;
 
 namespace VozAtiva.Application.DTOs;
 
-public class UserDTO
-{
-    public required string Name { get; set; }
-
-    public required string Email { get; set; }
-    public required string FederalCodeClient { get; set; }
-    public required DateTime Birthdate { get; set; }
-    public required string Phone { get; set; }
-    public required UserTypeEnum UserType { get; set; }
-}
+public record UserDTO(
+    string Name,
+    string Email,
+    string FederalCodeClient,
+    DateTime Birthdate,
+    string Phone,
+    UserTypeEnum UserType
+);

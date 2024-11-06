d33bbdccd596c88ca519f288beec441c59baf1e8
GuiOSousa <guilherme.olivesousa@gmail.com>
2024-11-06 12:30:13 -0300

    Implemented UserService

--- VozAtiva.Application/Services/Interfaces/IService.cs
+++ VozAtiva.Application/Services/Interfaces/IService.cs
@@ -3,7 +3,7 @@ using VozAtiva.Application.DTOs;
 namespace VozAtiva.Application.Services.Interfaces;
 
 public interface IService <TId, Tdto> {
-    Task<IEnumerable<Tdto>> Get();
+    Task<IEnumerable<Tdto>> GetAll();
     Task<Tdto> GetById(TId id);
     Task<Tdto> Add(Tdto dto);
     Task Update(Tdto dto);

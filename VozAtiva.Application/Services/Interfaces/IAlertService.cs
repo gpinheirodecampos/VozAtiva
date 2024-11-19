c2f86540eb27ad5a741575b94392a4e54c60cc09
Willy Gutsche <willy.gutsche@unifesp.br>
2024-11-19 12:30:17 -0300

    Set method returns in IAlertService

--- VozAtiva.Application/Services/Interfaces/IAlertService.cs
+++ VozAtiva.Application/Services/Interfaces/IAlertService.cs
@@ -4,7 +4,7 @@ namespace VozAtiva.Application.Services.Interfaces;
 
 public interface IAlertService: IService <Guid, AlertDTO> {
   Task<AlertDTO> GetByTitle(string Title);
-  Task<AlertDTO> GetByDate(DateTime Date);
-  Task<AlertDTO> GetByPublicAgentId(int PublicAgentId);
-  Task<AlertDTO> GetByAlertTypeId(int AlertTypeId);
+  Task<IEnumerable<AlertDTO>> GetByDate(DateTime Date);
+  Task<IEnumerable<AlertDTO>> GetByPublicAgentId(int PublicAgentId);
+  Task<IEnumerable<AlertDTO>> GetByAlertTypeId(int AlertTypeId);
 }

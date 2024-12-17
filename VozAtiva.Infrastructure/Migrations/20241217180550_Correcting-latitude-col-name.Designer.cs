4bab8ef186612293540426b98eac0369fde6ac9d
Hanyl <hanyelb46@gmail.com>
2024-12-17 15:08:41 -0300

    adicionada migração do banco de dados com correção do nome da coluna de longitude da entidade Alert

--- VozAtiva.Infrastructure/Migrations/20241217180550_Correcting-latitude-col-name.Designer.cs
+++ VozAtiva.Infrastructure/Migrations/20241217180550_Correcting-latitude-col-name.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VozAtiva.Infrastructure.Context;
@@ -11,9 +12,11 @@ using VozAtiva.Infrastructure.Context;
 namespace VozAtiva.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241217180550_Correcting-latitude-col-name")]
+    partial class Correctinglatitudecolname
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

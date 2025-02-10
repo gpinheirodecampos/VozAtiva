3f60940ed9b2125128ae2a1e198d4ee571016ca5
GutscheWilly <willy.gutsche@unifesp.br>
2025-02-10 16:24:33 -0300

    Resolvendo problema de criação dos alertas

--- VozAtiva.Application/Services/AlertService.cs
+++ VozAtiva.Application/Services/AlertService.cs
@@ -23,16 +23,6 @@ public class AlertService(IUnitOfWork unitOfWork, IMapper mapper, ISendEmailServ
 
     public async Task<AlertDTO> Add(AlertDTO dto)
     {
-        if (GetByPublicAgentId(dto.PublicAgentId) == null)
-        {
-            throw new Exception("Id do agente publico não encontrado.");
-        }
-
-        if (GetByAlertTypeId(dto.AlertTypeId) == null)
-        {
-            throw new Exception("Id do tipo de alerta não encontrado.");
-        }
-
         var alert = mapper.Map<Alert>(dto);
 
         await unitOfWork.AlertRepository.AddAsync(alert);
@@ -42,7 +32,6 @@ public class AlertService(IUnitOfWork unitOfWork, IMapper mapper, ISendEmailServ
         var user = await unitOfWork.UserRepository.GetByPropertyAsync(u => u.Id == alert.UserId);
 
         await emailService.EnqueueSendEmailAsync(alert.Id, user.Name, user.Email);
-
         return dto;
     }
 
